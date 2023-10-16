c7fbfd45f119ccce7a9e4c2c480b6937781d643d
riadas <riadas@mit.edu>
2023-10-16 10:57:42 -0400

    added explicit max-line-length param to flake8

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -4,4 +4,7 @@ set -euxo pipefail
 mypy --ignore-missing-imports cities/
 isort --check --profile black --diff cities/ tests/
 black --check cities/ tests/
-flake8 cities/ tests/
+# stop the build if there are Python syntax errors or undefined names
+flake8 cities/ tests/ --count --select=E9,F63,F7,F82 --show-source --statistics
+# exit-zero treats all errors as warnings. The GitHub editor is 127 chars wide
+flake8 cities/ tests/ --count --exit-zero --max-complexity=10 --max-line-length=127 --statistics
