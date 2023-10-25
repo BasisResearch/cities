63031ac5c3ff2bb95eddddf4124592a8232ebe0a
Niklewa <nikodemlewandowski@gmail.com>
2023-10-25 14:17:34 +0200

    adding urbanization cleaning pipeline

--- cities/utils/cleaning_pipeline.py
+++ cities/utils/cleaning_pipeline.py
@@ -1,10 +1,12 @@
 from cities.utils.clean_ethnic_composition import clean_ethnic_composition
 from cities.utils.clean_gdp import clean_gdp
+from cities.utils.clean_industry import clean_industry
 from cities.utils.clean_population import clean_population
 from cities.utils.clean_spending_commerce import clean_spending_commerce
 from cities.utils.clean_spending_HHS import clean_spending_HHS
 from cities.utils.clean_spending_transportation import clean_spending_transportation
 from cities.utils.clean_transport import clean_transport
+from cities.utils.clean_urbanization import clean_urbanization
 
 clean_gdp()
 
@@ -19,3 +21,7 @@ clean_spending_commerce()
 clean_spending_HHS()
 
 clean_ethnic_composition()
+
+clean_industry()
+
+clean_urbanization()
