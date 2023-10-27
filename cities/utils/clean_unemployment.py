698ad2b996069a1e6ee9433e46ff98e9a0e11ab9
Emily <emily@basis.ai>
2023-10-27 16:52:09 -0400

    unempl excludes 1 fips (2282), re-generate all csv

--- cities/utils/clean_unemployment.py
+++ cities/utils/clean_unemployment.py
@@ -2,54 +2,74 @@ import numpy as np
 import pandas as pd
 from pathlib import Path
 import os
+import pickle
 
 from cities.utils.cleaning_utils import standardize_and_scale
 from cities.utils.data_grabber import DataGrabber
+from cities.utils.clean_gdp import clean_gdp
 
 path = Path(__file__).parent.absolute()
+def clean_timeseries(variable_name, path_to_raw_csv): 
+    # function for cleaning a generic timeseries csv, wide format with these columns:
+    # GeoFIPS, GeoName, 2001, 2002, 2003, 2004, 2005, 2006, 2007, ...
 
-def clean_unemployment():
+    # load gdb, to get list of current non-excluded FIPS codes
     data = DataGrabber()
     data.get_features_wide(["gdp"])
     gdp = data.wide["gdp"]
 
-    dtype_mapping = {"STATE": str, "COUNTY": str}
-    unemployment_rate = pd.read_csv(os.path.join(path, "../../data/raw/unemployment_rate_wide_withNA.csv"), dtype=dtype_mapping)
-    print('hello')
+    # load raw csv
+    variable_db = pd.read_csv(path_to_raw_csv)
+    variable_db["GeoFIPS"] = variable_db["GeoFIPS"].astype(int)
+
+    # check if there are any counties that are missing from unempl but in gdp
+    # if so, add them to exclusions, and re-run gdp with new exclusions
+    if len(np.setdiff1d(gdp["GeoFIPS"].unique(), variable_db["GeoFIPS"].unique())) > 0:
+        # add new exclusions
+        new_exclusions = np.setdiff1d(gdp["GeoFIPS"].unique(), variable_db["GeoFIPS"].unique())
+        print("Adding new exclusions to exclusions.pkl: "+str(new_exclusions))
+        # open exclusions file
+        with open("../data/raw/exclusions.pkl", "rb") as file:
+            exclusions = pickle.load(file)
+        exclusions["transport"] = np.append(exclusions["transport"], new_exclusions)
+        with open("../data/raw/exclusions.pkl", "wb") as file:
+            pickle.dump(exclusions, file)
+        print("Rerunning gdp cleaning with new exclusions")
+        # rerun gdp cleaning
+        clean_gdp()
+        clean_timeseries(variable_name, path_to_raw_csv)
+        return
     
-    unemployment_rate["GeoFIPS"] = unemployment_rate["GeoFIPS"].astype(int)
-
-    common_fips = np.intersect1d(
-        gdp["GeoFIPS"].unique(), unemployment_rate["GeoFIPS"].unique()
+    # restrict to only common FIPS codes
+    common_fips = np.intersect1d(gdp["GeoFIPS"].unique(), variable_db["GeoFIPS"].unique())
+    variable_db = variable_db[variable_db["GeoFIPS"].isin(common_fips)]
+    variable_db = variable_db.merge(
+        gdp[["GeoFIPS", "GeoName"]], on=["GeoFIPS", "GeoName"], how="left"
     )
+    variable_db = variable_db.sort_values(by=["GeoFIPS", "GeoName"])
 
-    unemployment_rate = unemployment_rate[unemployment_rate["GeoFIPS"].isin(common_fips)]
-    
-    unemployment_rate = unemployment_rate.merge(
-        gdp[["GeoFIPS", "GeoName"]], on=["GeoFIPS", "GeoName"], how="outer"
-    )
-    print(unemployment_rate.head())
-    unemployment_rate = unemployment_rate.sort_values(by=["GeoFIPS", "GeoName"])
-
-    unemployment_rate_wide = unemployment_rate.copy()
-
-    unemployment_rate_long = pd.melt(
-        unemployment_rate,
+    # save 4 formats to .csv
+    variable_db_wide = variable_db.copy()
+    variable_db_long = pd.melt(
+        variable_db,
         id_vars=["GeoFIPS", "GeoName"],
         var_name="Year",
         value_name="Value",
     )
-
-    unemployment_rate_std_wide = standardize_and_scale(unemployment_rate)
-
-    unemployment_rate_std_long = pd.melt(
-        unemployment_rate_wide.copy(),
+    variable_db_std_wide = standardize_and_scale(variable_db)
+    variable_db_std_long = pd.melt(
+        variable_db_wide.copy(),
         id_vars=["GeoFIPS", "GeoName"],
         var_name="Year",
         value_name="Value",
     )
+    variable_db_wide.to_csv(os.path.join(path, "../../data/processed/" + variable_name + "_wide.csv"), index=False)
+    variable_db_long.to_csv(os.path.join(path, "../../data/processed/" + variable_name + "_long.csv"), index=False)
+    variable_db_std_wide.to_csv(os.path.join(path, "../../data/processed/" + variable_name + "_std_wide.csv"), index=False)
+    variable_db_std_long.to_csv(os.path.join(path, "../../data/processed/" + variable_name + "_std_long.csv"), index=False)
 
-    unemployment_rate_wide.to_csv(os.path.join(path, "../../data/processed/unemployment_rate_wide.csv"), index=False)
-    unemployment_rate_long.to_csv(os.path.join(path, "../../data/processed/unemployment_rate_long.csv"), index=False)
-    unemployment_rate_std_wide.to_csv(os.path.join(path, "../../data/processed/unemployment_rate_std_wide.csv"), index=False)
-    unemployment_rate_std_long.to_csv(os.path.join(path, "../../data/processed/unemployment_rate_std_long.csv"), index=False)
+
+def clean_unemployment():
+    variable_name = "unemployment_rate"
+    path_to_raw_csv = os.path.join(path, "../../data/raw/unemployment_rate_wide_withNA.csv")
+    clean_timeseries(variable_name, path_to_raw_csv)
