d1fe7e9b93a06a85e9e92237110652a9f4a6ad0b
rfl-urbaniak <rfl.urbaniak@gmail.com>
2023-10-31 17:14:43 +0100

    data notebook cleanup, lint

--- cities/utils/cleaning_pipeline.py
+++ cities/utils/cleaning_pipeline.py
@@ -1,13 +1,13 @@
 from cities.utils.clean_ethnic_composition import clean_ethnic_composition
 from cities.utils.clean_gdp import clean_gdp
 from cities.utils.clean_industry import clean_industry
+from cities.utils.clean_industry_ts import clean_industry_ts
 from cities.utils.clean_population import clean_population
 from cities.utils.clean_spending_commerce import clean_spending_commerce
 from cities.utils.clean_spending_HHS import clean_spending_HHS
 from cities.utils.clean_spending_transportation import clean_spending_transportation
 from cities.utils.clean_transport import clean_transport
 from cities.utils.clean_urbanization import clean_urbanization
-from cities.utils.clean_industry_ts import clean_industry_ts    
 
 clean_gdp()
 
