6b428eacd5f328d26850c715fd7bdcd59c6e1b7d
rfl-urbaniak <rfl.urbaniak@gmail.com>
2023-11-13 17:32:56 -0500

    building model F into repo WIP

---
+++ cities/modeling/model_interactions.py
@@ -0,0 +1,74 @@
+import torch
+import pyro
+import pyro.distributions as dist
+
+def cities_model_interactions(N_t, N_cov, N_s, N_u, N_obs, state_index_sparse, state_index, time_index, unit_index, leeway = .2):
+    
+    Y_bias = pyro.sample("Y_bias", dist.Normal(0, leeway))
+    T_bias = pyro.sample("T_bias", dist.Normal(0, leeway))
+
+    weight_TY = pyro.sample("weight_TY", dist.Normal(0, leeway))
+    
+    
+    sigma_T = pyro.sample("sigma_T", dist.Exponential(1))
+    sigma_Y = pyro.sample("sigma_Y", dist.Exponential(1))
+
+    observations_plate = pyro.plate("observations_plate", N_obs, dim = -1)
+
+    counties_plate = pyro.plate("counties_plate", N_u, dim = -2)
+    states_plate = pyro.plate("states_plate", N_s, dim=-3)
+    covariates_plate = pyro.plate("covariates_plate", N_cov, dim=-4)
+    time_plate = pyro.plate("time_plate", N_t, dim=-5)
+
+
+    with covariates_plate:
+        X_bias = pyro.sample("X_bias", dist.Normal(0, leeway)).squeeze()
+        sigma_X = pyro.sample("sigma_X", dist.Exponential(1)).squeeze()
+        weight_XT = pyro.sample("weight_XT", dist.Normal(0, leeway)).squeeze()
+        weight_XY = pyro.sample("weight_XY", dist.Normal(0, leeway)).squeeze()
+
+
+    with states_plate:
+        weight_UsT = pyro.sample("weight_UsT", dist.Normal(0, leeway)).squeeze()      
+        weight_UsY = pyro.sample("weight_UsY", dist.Normal(0, leeway)).squeeze()        
+
+        with covariates_plate:
+            weight_UsX = pyro.sample("weight_UsX", dist.Normal(0, leeway)).squeeze()
+    
+
+    with time_plate:
+        weight_UtT = pyro.sample("weight_UtT", dist.Normal(0, leeway)).squeeze()
+        weight_UtY = pyro.sample("weight_UtY", dist.Normal(0, leeway)).squeeze()
+
+   
+    with counties_plate:
+        
+        UsX_weight_selected = weight_UsX.squeeze().T.squeeze()[state_index_sparse]     
+        X_means = torch.einsum("c,uc->uc" ,X_bias, UsX_weight_selected)
+        X = pyro.sample("X", dist.Normal(X_means, sigma_X)).squeeze()
+
+
+    XT_weighted = torch.einsum("uc, c -> u",X, weight_XT)
+    XY_weighted = torch.einsum("uc, c -> u", X, weight_XY) 
+    
+    with observations_plate:
+
+        T_mean = (T_bias + weight_UtT[time_index] 
+                  + weight_UsT[state_index]
+                  + XT_weighted[unit_index]
+                  )
+        
+        T =  pyro.sample("T", dist.Normal(T_mean, sigma_T))
+
+    
+        Y_mean = (Y_bias + weight_UtY[time_index] 
+                + weight_UsY[state_index] 
+                + weight_TY * T 
+                + XY_weighted[unit_index]
+                 )   
+
+        Y = pyro.sample("Y", dist.Normal(Y_mean, sigma_Y))
+
+    return 
+
+
---
+++ cities/modeling/modeling_utils.py
@@ -0,0 +1,156 @@
+import torch
+import pyro
+from chirho.observational.handlers import condition
+import seaborn as sns
+import pandas as pd
+
+from cities.utils.data_grabber import DataGrabber, list_available_features, list_tensed_features
+
+
+
+def prep_data_for_interaction_inference(outcome_dataset, intervention_dataset, intervention_variable, forward_shift):
+    
+    dg = DataGrabber()
+
+    tensed_covariates_datasets = [var for var in list_tensed_features() if var not in [outcome_dataset, intervention_dataset]]
+    fixed_covariates_datasets = [var for var in list_available_features() if var not in tensed_covariates_datasets + [outcome_dataset, intervention_dataset]]
+
+    # TODO revise once transformations are available
+    dg.get_features_std_long(list_available_features()) 
+    dg.get_features_std_wide(list_available_features()) 
+
+    year_min = max(dg.std_long[intervention_dataset]['Year'].min(), dg.std_long[outcome_dataset]['Year'].min())
+    year_max = min(dg.std_long[intervention_dataset]['Year'].max(), dg.std_long[outcome_dataset]['Year'].max())
+    outcome_df = dg.std_long[outcome_dataset].sort_values(by=['GeoFIPS', 'Year'])
+
+    # now we adding forward shift to the outcome
+    # cleaning up and puting intervention/outcome in one df
+    # and fixed covariates in another
+
+    outcome_df[f'{outcome_dataset}_shifted_by_{forward_shift}'] = None
+
+    geo_subsets = []
+    for geo_fips in outcome_df['GeoFIPS'].unique():
+        geo_subset = outcome_df[outcome_df['GeoFIPS'] == geo_fips].copy()
+        # Shift the 'Value' column `forward_shift` in a new column
+        geo_subset[f'{outcome_dataset}_shifted_by_{forward_shift}'] = geo_subset['Value'].shift(-forward_shift)
+        geo_subsets.append(geo_subset)
+        
+    outcome_df = pd.concat(geo_subsets).reset_index(drop=True)
+
+    outcome = outcome_df[(outcome_df['Year'] >= year_min) & (outcome_df['Year'] <= year_max + forward_shift)]
+    
+    intervention = dg.std_long[intervention_dataset][(dg.std_long[intervention_dataset]['Year'] >= year_min) & (dg.std_long[intervention_dataset]['Year'] <= year_max)]    
+    f_covariates = {dataset: dg.std_wide[dataset] for dataset in fixed_covariates_datasets}
+    f_covariates_joint = f_covariates[fixed_covariates_datasets[0]]
+    for dataset in f_covariates.keys():
+        if dataset != fixed_covariates_datasets[0]:
+            if 'GeoName' in f_covariates[dataset].columns:
+                f_covariates[dataset] = f_covariates[dataset].drop(columns=['GeoName'])
+            f_covariates_joint = f_covariates_joint.merge(f_covariates[dataset], on=['GeoFIPS'])
+
+
+    i_o_data = pd.merge(outcome, intervention, on=['GeoFIPS', 'Year'])
+
+    if 'GeoName_x' in i_o_data.columns:
+        i_o_data.rename(columns={'GeoName_x': "GeoName"}, inplace=True)    
+        columns_to_drop = i_o_data.filter(regex=r'^GeoName_[a-zA-Z]$')
+        i_o_data.drop(columns=columns_to_drop.columns, inplace=True)
+
+    i_o_data.rename(columns={'Value': outcome_dataset}, inplace=True)
+
+    i_o_data['state'] = [code // 1000 for code in i_o_data['GeoFIPS']]
+
+    N_s = len(i_o_data['state'].unique())  #number of states
+    i_o_data.dropna(inplace=True)
+
+
+    i_o_data['unit_index']= pd.factorize(i_o_data['GeoFIPS'].values)[0]
+    i_o_data['state_index']= pd.factorize(i_o_data['state'].values)[0]
+    i_o_data['time_index']= pd.factorize(i_o_data['Year'].values)[0]
+
+
+    assert i_o_data['GeoFIPS'].isin(f_covariates_joint['GeoFIPS']).all()
+
+    f_covariates_joint.drop(columns=['GeoName'], inplace=True)
+    data = i_o_data.merge(f_covariates_joint, on='GeoFIPS', how='left')
+
+    assert not data.isna().any().any()
+
+    time_index_idx = data.columns.get_loc('time_index')
+    covariates_df = data.iloc[:, time_index_idx + 1:].copy()
+    covariates_df_sparse = covariates_df.copy()
+    covariates_df_sparse['unit_index'] = data['unit_index']
+    covariates_df_sparse['state_index'] = data['state_index']
+    covariates_df_sparse.drop_duplicates(inplace=True)
+    assert set(covariates_df_sparse['unit_index']) == set(data['unit_index'])
+
+    # get tensors
+
+    if torch.cuda.is_available():
+        device = torch.device("cuda")
+    else:
+        device = torch.device("cpu")
+
+    y = data[f'{outcome_dataset}_shifted_by_{forward_shift}']
+    y = torch.tensor(y, dtype=torch.float32, device = device) 
+
+    unit_index = torch.tensor(data['unit_index'], dtype=torch.int, device = device)
+    unit_index_sparse = torch.tensor(covariates_df_sparse['unit_index'], dtype=torch.int)
+
+    state_index = torch.tensor(data['state_index'], dtype=torch.int, device = device)
+    state_index_sparse = torch.tensor(covariates_df_sparse['state_index'], dtype=torch.int)
+
+    time_index = torch.tensor(data['time_index'], dtype=torch.int, device = device)
+    intervention = torch.tensor(data[intervention_variable], dtype=torch.float32, device = device)
+
+    covariates = torch.tensor(covariates_df.values, dtype = torch.float32, device = device)
+
+    covariates_df_sparse.drop(columns=['unit_index','state_index'], inplace=True)
+    covariates_sparse = torch.tensor(covariates_df_sparse.values, dtype = torch.float32, device = device)
+
+    N_cov = covariates.shape[1] #number of covariates
+    N_u = covariates_sparse.shape[0] #number of units (counties)
+    N_obs = len(y) #number of observations
+    N_t = len(time_index.unique()) #number of time points
+    N_s = len(state_index.unique()) #number of states
+
+    assert len(intervention) == len(y)
+    assert len(unit_index) == len(y)
+    assert len(state_index) == len(unit_index)
+    assert len(time_index) == len(unit_index)
+    assert covariates.shape[1] == covariates_sparse.shape[1]
+    assert len(unit_index_sparse) == N_u
+
+    return {"N_t": N_t, "N_cov": N_cov, "N_s": N_s, "N_u": N_u, "N_obs": N_obs,
+            "unit_index": unit_index, "state_index": state_index, "time_index": time_index,
+            "unit_index_sparse": unit_index_sparse, "state_index_sparse": state_index_sparse,
+            "covariates": covariates, "covariates_sparse": covariates_sparse,
+            "intervention": intervention, "y": y}
+
+
+
+
+
+
+def plot_predictive(model, data, intervention_variable, outcome_dataset, forward_shift, guide = None, **kwargs):
+
+    model = condition(data=dict(**kwargs))(model)
+    
+    if guide:
+       guide_tr = pyro.poutine.trace(guide).get_trace()
+       model_tr = pyro.poutine.trace(pyro.poutine.replay(model(T_outcome = T_outcome, N_states = N_states, state_index = state_index,
+                               time_index = time_index, intervention = intervention), trace=guide_tr)).get_trace()
+    else:
+        with pyro.poutine.trace() as tr:
+            model(**kwargs)
+
+    model_tr = tr.trace
+    y = model_tr.nodes['y']['value']
+
+    predictive_copy = data.copy()
+
+    predictive_copy['post_test'] = y.reshape(-1, 1).detach().numpy()  
+
+    sns.pairplot(predictive_copy[[intervention_variable, f"{outcome_dataset}_shifted_by_{forward_shift}", 'post_test']])
+    
