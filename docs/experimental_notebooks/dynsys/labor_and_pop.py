2b1ccc6c9058d76ecacbabc2f778618c2a6098fc
Andy Zane <andy@basis.ai>
2023-11-20 18:03:43 -0700

    adds labor force change model and eyeball tests.

--- docs/experimental_notebooks/dynsys/labor_and_pop.py
+++ docs/experimental_notebooks/dynsys/labor_and_pop.py
@@ -71,35 +71,35 @@ class FluxDynamics(pyro.nn.PyroModule):
 class TotalLaborForceDynamics(FluxDynamics):
 
     def __init__(self, *args, total_labor_force_f_of_t, lf_fluxes, **kwargs):
+        super().__init__(*args, **kwargs)
+
         # The tlf function takes in time as a scalar and returns a state vector representing
         #  the total labor force across all fips.
         self.tlf = total_labor_force_f_of_t
         self.lf_fluxes = lf_fluxes  # .shape == (n_cats, n_fips)
 
-        super().__init__(*args, **kwargs)
-
     @pyro.nn.pyro_method
     def diff(self, dstate: State[torch.Tensor], state: State[torch.Tensor]) -> None:
         super().diff(dstate, state)
-        t = state['t']
-        dpopdt = torch.autograd.grad(self.tlf(t), t)[0]  # .shape == (n_fips,)
+        t = state['t'].detach().requires_grad_(True)
+        dtlfdt = torch.autograd.functional.jacobian(self.tlf, t) # .shape == (n_fips,)
 
         # For a given county, the labor force deltas are distributed across the employment categories,
         #  so these need to be normalized across categories.
         normed_lf_fluxes = self.lf_fluxes / self.lf_fluxes.sum(dim=0, keepdim=True)
 
-        # Note that this vectorization is over (n_fips,), so flux here won't be normalized.
-        for flux, k in zip(normed_lf_fluxes, state.keys()):
-            dstate[k] += flux * dpopdt
+        # Broadcast the total labor force delta across the flux proportions for each of the categories.
+        dstate["fluxers"] += normed_lf_fluxes * dtlfdt[None, :]
 
 
 def transform_county_fluxes(county_flux, end_scale):
     return torch.sigmoid(county_flux) * end_scale
 
 
-def flux_prior(n_fips, n_states, n_pairs, mapped_state_idx):
+def flux_prior(n_fips, n_states, n_cats, mapped_state_idx):
 
-    flux_plate = pyro.plate("flux_plate", n_pairs, dim=-3)
+    src_cat_plate = pyro.plate("src_cat_plate", n_cats, dim=-4)
+    dst_cat_plate = pyro.plate("dst_cat_plate", n_cats, dim=-3)
     states_plate = pyro.plate("states_plate", n_states, dim=-2)
     counties_plate = pyro.plate("counties_plate", n_fips, dim=-1)
 
@@ -110,26 +110,28 @@ def flux_prior(n_fips, n_states, n_pairs, mapped_state_idx):
     # After pushing through a sigmoid, this scales things from unit range.
     end_scale = 0.1
 
-    with flux_plate:
-        federal_flux = pyro.sample("federal_flux", dist.Normal(0., scale))
+    with src_cat_plate:
+        with dst_cat_plate:
+            # TODO mask diagonal along categories.
+            federal_flux = pyro.sample("federal_flux", dist.Normal(0., scale))
 
-        with states_plate:
-            state_flux = pyro.sample("state_flux", dist.Normal(federal_flux, scale))
+            with states_plate:
+                state_flux = pyro.sample("state_flux", dist.Normal(federal_flux, scale))
 
-        with counties_plate:
-            # Could extend this with a mapped_obs_idx as well, and index into the last dimension.
-            mapped_county_flux = state_flux[:, mapped_state_idx, ...]
-            # Now, transpose because the county and state dimensions effectively get swapped by
-            #  indexing into the state dimension.
-            mapped_county_flux = mapped_county_flux.transpose(states_plate.dim, counties_plate.dim)
-            county_flux = pyro.sample("county_flux", dist.Normal(mapped_county_flux, scale))
+            with counties_plate:
+                # Could extend this with a mapped_obs_idx as well, and index into the last dimension.
+                mapped_county_flux = state_flux[:, :, mapped_state_idx, ...]
+                # Now, transpose because the county and state dimensions effectively get swapped by
+                #  indexing into the state dimension.
+                mapped_county_flux = mapped_county_flux.transpose(states_plate.dim, counties_plate.dim)
+                county_flux = pyro.sample("county_flux", dist.Normal(mapped_county_flux, scale))
 
     # The fips codes are "flattened" wrt states, so just remove them, but note that the
     #  state flux sample still centers the county flux for counties in that state.
     # Returning counties plate as well so that initial_state sampling can use it. Kind of strange.
     county_flux = county_flux.squeeze(dim=states_plate.dim)
 
-    return transform_county_fluxes(county_flux, end_scale), counties_plate
+    return transform_county_fluxes(county_flux, end_scale), src_cat_plate, dst_cat_plate, states_plate, counties_plate
 
 
 def full_prior(employment_classes, fips_codes):
@@ -139,38 +141,77 @@ def full_prior(employment_classes, fips_codes):
     # Get a tensor that gives you the index of each mapped state code in the unique state codes.
     mapped_state_idx = tnsr([unique_state_codes.index(c) for c in mapped_state_codes])
 
-    # Get each pair of employment categories.
-    pairs = [pair for pair in product(employment_classes, employment_classes) if pair[0] != pair[1]]
-
-    fluxes, counties_plate = flux_prior(
+    fluxes, src_cat_plate, dst_cat_plate, states_plate, counties_plate = flux_prior(
         n_fips=len(fips_codes),
         n_states=len(unique_state_codes),
-        n_pairs=len(pairs),
+        n_cats=len(employment_classes),
         mapped_state_idx=mapped_state_idx
     )
 
-    with counties_plate:
-        initial_state = State(**{
-            ec: pyro.sample(f"{ec}0", dist.Uniform(0.5, 1.0)) for ec in employment_classes
-        })
+    with src_cat_plate:
+        with counties_plate:
+            initial_state = State(
+                fluxers=pyro.sample('fluxers0', dist.Uniform(0.5, 1.0))  # .shape == (n_cats, n_fips)
+                .squeeze(dst_cat_plate.dim, states_plate.dim)  # TODO HACK eh...might be worth making new plate?
+            )
 
-        # pop_growth = pyro.sample("pop_growth", dist.Normal(0.0, 0.1))
-        # pop_start =
+            # The fixed proportions of labor force growth that go to each of the classes.
+            # Normalization is handled downstream. Total labor force growth is defined below.
+            lf_fluxes = pyro.sample("lf_fluxes", dist.Uniform(0.1, 1.0)) \
+                .squeeze(dst_cat_plate.dim, states_plate.dim)
 
-    # fdyns = TotalLaborForceDynamics(
-    #     fluxes,
-    #     pairs,
-    #     total_labor_force_f_of_t=,
-    #     lf_fluxes=,
-    #     noise_scale = .05
-    # )
-    fdyns = FluxDynamics(
-        fluxes,
-        pairs,
-        noise_scale=0.05
+    # with counties_plate:
+    # TODO HACK because the original counties plate resulted in a scalar sample? Even though definition is
+    #  identical.
+    with pyro.plate("counties_plate_", len(fips_codes), dim=-1):
+        # Labor force growth in percent per year. This is specified per county only, and then distributed
+        #  to employment classes via lf_fluxes.
+        lf_growth = pyro.sample("lf_growth", dist.Normal(0.0, 0.01))
+
+    # For consistency, total labor force at t=0 is sum fluxers (employment classes) at t=0.
+    tlf_start = initial_state["fluxers"].sum(dim=0)
+
+    fdyns = TotalLaborForceDynamics(
+        flux_rates=fluxes,
+        total_labor_force_f_of_t=lambda t: tlf_start * torch.exp(lf_growth * t),
+        lf_fluxes=lf_fluxes,
+        noise_scale=.05
     )
 
     return fdyns, initial_state
+
+
+def pop_dynsys_only():
+    employment_classes = ["agriculture", "knowledge", "healthcare"]
+    _colors = ["#1f77b4", "#ff7f0e", "#2ca02c"]
+    colors = dict(zip(employment_classes, _colors))
+    # fips_codes = [1001, 1005, 2001, 2003, 3005]
+    fips_codes = [1001, 2002]
+
+    fdyns, initial_state = full_prior(employment_classes, fips_codes)
+
+    start_time = tnsr(0.0)
+    end_time = tnsr(30.0)
+    step_size = tnsr(0.1)
+    # noinspection PyTypeChecker
+    trajectory_times = torch.arange(start_time + step_size, end_time, step_size)
+
+    with LogTrajectory(trajectory_times) as traj:
+        simulate(fdyns, initial_state, start_time, end_time, solver=TorchDiffEq(), method="dopri8")
+
+    for fc in range(len(fips_codes)):
+        plt.figure()
+        for i in range(len(employment_classes)):
+            plt.plot(traj.times, traj.trajectory["fluxers"][i, fc], color=colors[employment_classes[i]])
+
+        # Also plot the sum of the fluxers for each fc, alongside the tlf curve.
+        plt.plot(traj.times, traj.trajectory["fluxers"][:, fc].sum(dim=0), color="black")
+        plt.plot(traj.times, torch.stack([fdyns.tlf(t)[fc] for t in traj.times]), color="red", linestyle="--")
+
+        plt.ylim(0, len(employment_classes))
+
+    plt.show()
+
 
 
 def run_svi_inference(model, n_steps=10, verbose=250, lr=0.03, **model_kwargs):
@@ -239,7 +280,7 @@ def dynsys_only():
     _colors = ["#1f77b4", "#ff7f0e", "#2ca02c"]
     colors = dict(zip(employment_classes, _colors))
     # fips_codes = [1001, 1005, 2001, 2003, 3005]
-    fips_codes = [1001, 1002]
+    fips_codes = [1001, 2002]
 
     flux_rates = torch.rand(
         len(employment_classes),
@@ -247,6 +288,13 @@ def dynsys_only():
         len(fips_codes)
         # With diagonal zeroed, as self flux terms aren't necessary.
     ) * (1. - torch.eye(len(employment_classes)))[:, :, None]
+
+
+    # Real flux rates
+    real_flux_rates = full_prior(employment_classes, fips_codes)
+
+
+    return
 
     assert (flux_rates >= 0).all()
 
@@ -277,4 +325,5 @@ def dynsys_only():
 
 if __name__ == "__main__":
     # main()
-    dynsys_only()
+    # dynsys_only()
+    pop_dynsys_only()
