43c4290638ceb6f8accdb2e9905cfe5287e90607
rfl-urbaniak <rfl.urbaniak@gmail.com>
2023-12-20 20:19:56 +0100

    lint notebooks

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -6,6 +6,6 @@ isort --check --profile black --diff cities/ tests/
 black --check cities/ tests/
 flake8 cities/ tests/ --ignore=E203,W503 --max-line-length=127
 
-nbqa black -v --check docs/guides/
+nbqa black --check docs/guides/
 nbqa autoflake -v --recursive --check docs/guides/
 nbqa isort --check docs/guides/
