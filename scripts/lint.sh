4b4cfc89770067d2e2e2d808b50131921ea5a177
rfl-urbaniak <rfl.urbaniak@gmail.com>
2023-12-20 20:02:52 +0100

    added notebook linting to lint.sh

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -5,3 +5,7 @@ mypy --ignore-missing-imports cities/
 isort --check --profile black --diff cities/ tests/
 black --check cities/ tests/
 flake8 cities/ tests/ --ignore=E203,W503 --max-line-length=127
+
+nbqa black --check docs/guides/
+nbqa autoflake --recursive --check docs/guides/
+nbqa isort --check docs/guides/
