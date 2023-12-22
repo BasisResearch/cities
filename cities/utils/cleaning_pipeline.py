867b097d4c7616dde36b63ca0cf3cbf94f7f6fe8
Niklewa <nikodemlewandowski@gmail.com>
2023-12-22 12:40:58 +0100

    added ethnic_composition_ma

--- cities/utils/cleaning_pipeline.py
+++ cities/utils/cleaning_pipeline.py
@@ -1,39 +1,42 @@
-from cities.utils.clean_ethnic_composition import clean_ethnic_composition
-from cities.utils.clean_gdp import clean_gdp
-from cities.utils.clean_industry import clean_industry
-from cities.utils.clean_industry_ts import clean_industry_ts
-from cities.utils.clean_population import clean_population
-from cities.utils.clean_spending_commerce import clean_spending_commerce
-from cities.utils.clean_spending_HHS import clean_spending_HHS
-from cities.utils.clean_spending_transportation import clean_spending_transportation
-from cities.utils.clean_transport import clean_transport
-from cities.utils.clean_unemployment import clean_unemployment
-from cities.utils.clean_urbanization import clean_urbanization
-from cities.utils.cleaning_poverty import clean_poverty
-from cities.utils.clean_population_ma import clean_population_ma
+# from cities.utils.clean_ethnic_composition import clean_ethnic_composition
+# from cities.utils.clean_gdp import clean_gdp
+# from cities.utils.clean_industry import clean_industry
+# from cities.utils.clean_industry_ts import clean_industry_ts
+# from cities.utils.clean_population import clean_population
+# from cities.utils.clean_spending_commerce import clean_spending_commerce
+# from cities.utils.clean_spending_HHS import clean_spending_HHS
+# from cities.utils.clean_spending_transportation import clean_spending_transportation
+# from cities.utils.clean_transport import clean_transport
+# from cities.utils.clean_unemployment import clean_unemployment
+# from cities.utils.clean_urbanization import clean_urbanization
+# from cities.utils.cleaning_poverty import clean_poverty
+# from cities.utils.clean_population_ma import clean_population_ma
+from cities.utils.clean_ethnic_composition_ma import clean_ethnic_composition_ma
 
-clean_population_ma()
+clean_ethnic_composition_ma()
 
-clean_poverty()
+# clean_population_ma()
 
-clean_unemployment()
+# clean_poverty()
 
-clean_gdp()
+# clean_unemployment()
 
-clean_population()
+# clean_gdp()
 
-clean_transport()
+# clean_population()
 
-clean_spending_transportation()
+# clean_transport()
 
-clean_spending_commerce()
+# clean_spending_transportation()
 
-clean_spending_HHS()
+# clean_spending_commerce()
 
-clean_ethnic_composition()
+# clean_spending_HHS()
 
-clean_industry()
+# clean_ethnic_composition()
 
-clean_urbanization()
+# clean_industry()
 
-clean_industry_ts()
+# clean_urbanization()
+
+# clean_industry_ts()
