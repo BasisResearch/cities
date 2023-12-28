4e2d6181a2394e2b854058e72955f15d2ccf91b9
Niklewa <nikodemlewandowski@gmail.com>
2023-12-28 21:53:49 +0100

    adding gdp_ma and data documentation

--- cities/utils/cleaning_pipeline.py
+++ cities/utils/cleaning_pipeline.py
@@ -1,19 +1,22 @@
-# from cities.utils.clean_ethnic_composition import clean_ethnic_composition
-# from cities.utils.clean_gdp import clean_gdp
-# from cities.utils.clean_industry import clean_industry
-# from cities.utils.clean_industry_ts import clean_industry_ts
-# from cities.utils.clean_population import clean_population
-# from cities.utils.clean_spending_commerce import clean_spending_commerce
-# from cities.utils.clean_spending_HHS import clean_spending_HHS
-# from cities.utils.clean_spending_transportation import clean_spending_transportation
-# from cities.utils.clean_transport import clean_transport
-# from cities.utils.clean_unemployment import clean_unemployment
-# from cities.utils.clean_urbanization import clean_urbanization
-# from cities.utils.cleaning_poverty import clean_poverty
+from cities.utils.clean_ethnic_composition import clean_ethnic_composition
+from cities.utils.clean_gdp import clean_gdp
+from cities.utils.clean_industry import clean_industry
+from cities.utils.clean_industry_ts import clean_industry_ts
+from cities.utils.clean_population import clean_population
+from cities.utils.clean_spending_commerce import clean_spending_commerce
+from cities.utils.clean_spending_HHS import clean_spending_HHS
+from cities.utils.clean_spending_transportation import clean_spending_transportation
+from cities.utils.clean_transport import clean_transport
+from cities.utils.clean_unemployment import clean_unemployment
+from cities.utils.clean_urbanization import clean_urbanization
+from cities.utils.cleaning_poverty import clean_poverty
 from cities.utils.clean_population_ma import clean_population_ma
 from cities.utils.clean_ethnic_composition_ma import clean_ethnic_composition_ma
 from cities.utils.clean_urbanicity_ma import clean_urbanicity_ma
 from cities.utils.clean_industry_ma import clean_industry_ma
+from cities.utils.clean_gdp_ma import clean_gdp_ma
+
+clean_gdp_ma()
 
 clean_industry_ma()
 
@@ -23,26 +26,26 @@ clean_ethnic_composition_ma()
 
 clean_population_ma()
 
-# clean_poverty()
+clean_poverty()
 
-# clean_unemployment()
+clean_unemployment()
 
-# clean_gdp()
+clean_gdp()
 
-# clean_population()
+clean_population()
 
-# clean_transport()
+clean_transport()
 
-# clean_spending_transportation()
+clean_spending_transportation()
 
-# clean_spending_commerce()
+clean_spending_commerce()
 
-# clean_spending_HHS()
+clean_spending_HHS()
 
-# clean_ethnic_composition()
+clean_ethnic_composition()
 
-# clean_industry()
+clean_industry()
 
-# clean_urbanization()
+clean_urbanization()
 
-# clean_industry_ts()
+clean_industry_ts()
