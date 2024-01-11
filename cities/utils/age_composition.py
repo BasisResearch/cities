1edf76a1ee863224653fdae5256887714db3e890
Niklewa <nikodemlewandowski@gmail.com>
2024-01-11 15:04:43 +0100

    adding age composition WIP

---
+++ cities/utils/age_composition.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+
+from cities.utils.clean_variable import clean_variable
+from cities.utils.data_grabber import find_repo_root, DataGrabber
+
+root = find_repo_root()
+
+data = DataGrabber()
+data.get_features_wide(["gdp"])
+gdp = data.wide["gdp"]
+
+
+def clean_age_first():
+    
+    age = pd.read_csv(f"{root}/data/raw/age_composition.csv")
+
+    age.iloc[:, 2:] = age.iloc[:, 2:].div(age['total_pop'], axis=0) * 100
+    age.drop('total_pop', axis=1, inplace=True)    
+    
+    age.to_csv(f"{root}/data/raw/age_percentages.csv", index=False)
+
+def clean_age_composition():
+    clean_age_first()
+    
+    variable_name = "age_composition"
+    path_to_raw_csv = f"{root}/data/raw/age_percentages.csv"
+    
+    clean_variable(variable_name, path_to_raw_csv)
