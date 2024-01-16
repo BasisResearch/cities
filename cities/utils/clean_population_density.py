c96062398eea4caa283389935a2a38be8adcb330
Niklewa <nikodemlewandowski@gmail.com>
2024-01-16 10:49:25 +0100

    lint, data source updates

--- cities/utils/clean_population_density.py
+++ cities/utils/clean_population_density.py
@@ -7,6 +7,6 @@ root = find_repo_root()
 def clean_population_density():
     cleaner = VariableCleaner(
         variable_name="population_density",
-        path_to_raw_csv=f"{root}/data/raw/population_density.csv"
+        path_to_raw_csv=f"{root}/data/raw/population_density.csv",
     )
     cleaner.clean_variable()
