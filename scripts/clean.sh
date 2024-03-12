cfde8bcc84ba0db5a670cf21d970139d8c3018fc
rfl-urbaniak <rfl.urbaniak@gmail.com>
2024-03-12 19:31:48 +0100

    suspend notebook tests

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 set -euxo pipefail
 
-#isort cities/ tests/
+# isort suspended till the CI-vs-local issue is resolved
+# isort cities/ tests/
+
 black cities/ tests/
 autoflake --remove-all-unused-imports --in-place --recursive ./cities ./tests
 
 nbqa autoflake --remove-all-unused-imports --recursive --in-place docs/guides/ 
-#nbqa isort docs/guides/
+# nbqa isort docs/guides/
 nbqa black docs/guides/
 
