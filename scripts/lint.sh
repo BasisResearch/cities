0a37867d401427bf2d1c32783141e83166562c32
rfl-urbaniak <rfl.urbaniak@gmail.com>
2024-03-12 11:22:41 +0100

    dealing with linter versions

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -2,7 +2,7 @@
 set -euxo pipefail
 
 mypy --ignore-missing-imports cities/
-isort --check --profile black --diff cities/ tests/
+isort --profile black  --check --diff cities/ tests/
 black --check cities/ tests/
 flake8 cities/ tests/ --ignore=E203,W503 --max-line-length=127
 
