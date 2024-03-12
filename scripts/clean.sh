7f4735943ebd31984cb0bee87360555234b8f61b
rfl-urbaniak <rfl.urbaniak@gmail.com>
2024-03-12 10:48:47 +0100

    switch to --apply within isort in clean.sh

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -euxo pipefail
 
-isort --profile black cities/ tests/
+isort --profile black --apply cities/ tests/
 black cities/ tests/
 autoflake --remove-all-unused-imports --in-place --recursive ./cities ./tests
 
 nbqa black docs/guides/
 nbqa autoflake --remove-all-unused-imports --recursive --in-place docs/guides/ 
-nbqa isort -in-place docs/guides/
+nbqa isort --apply docs/guides/
