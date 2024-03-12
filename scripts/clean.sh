475c73243024c6fe0f039da502367531bf972297
rfl-urbaniak <rfl.urbaniak@gmail.com>
2024-03-12 14:53:40 +0100

    revising workflow

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -5,6 +5,7 @@ isort --profile black cities/ tests/
 black cities/ tests/
 autoflake --remove-all-unused-imports --in-place --recursive ./cities ./tests
 
-nbqa black docs/guides/
+
 nbqa autoflake --remove-all-unused-imports --recursive --in-place docs/guides/ 
-nbqa isort docs/guides/
+nbqa black docs/guides/
+
