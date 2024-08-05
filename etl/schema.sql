7cae0db02290f25b58a3f899e06ad36a0820254a
Jack Feser <jack.feser@gmail.com>
2024-08-05 11:06:01 -0400

    make comments visible in db

--- etl/schema.sql
+++ etl/schema.sql
@@ -13,12 +13,16 @@ create table parcel (
   , parcel_id text
   , parcel_year int not null
 
-  , parcel_emv_land numeric     -- Estimated Market Value, land
-  , parcel_emv_building numeric -- Estimated Market Value, building
-  , parcel_emv_total numeric    -- Estimated Market Value, total (may be more than sum of land and building)
+  , parcel_emv_land numeric
+  , parcel_emv_building numeric
+  , parcel_emv_total numeric
   , parcel_year_built int
   , parcel_sale_date date
   , parcel_sale_value numeric
 
   , parcel_geom_id int references parcel_geom(parcel_geom_id)
 );
+
+comment on column parcel.parcel_emv_land is 'Estimated Market Value, land';
+comment on column parcel.parcel_emv_building is 'Estimated Market Value, buildings';
+comment on column parcel.parcel_emv_total is 'Estimated Market Value, total (may be more than sum of land and building)';
