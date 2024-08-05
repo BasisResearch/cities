ad5dff511b0a923da3266c110646dd7db42ede93
Jack Feser <jack.feser@gmail.com>
2024-08-05 12:10:39 -0400

    remove table prefixes from field names

--- etl/load_parcels.py
+++ etl/load_parcels.py
@@ -19,7 +19,7 @@ distinct_geom = " union ".join(
     f"select geom from parcel_raw_{year} where upper({'city' if year < 2018 else 'ctu_name'}) = 'MINNEAPOLIS'"
     for year in PARCEL_YEARS
 )
-parcel_geom_load = f"insert into parcel_geom (parcel_geom_data) {distinct_geom};"
+parcel_geom_load = f"insert into parcel_geom (geom) {distinct_geom};"
 print("Executing:", parcel_geom_load)
 cur.execute(parcel_geom_load)
 conn.commit()
@@ -27,16 +27,16 @@ conn.commit()
 # insert parcel data into parcel table
 parcel_data = " union all ".join(
     f"""
-    select replace(pin, '{COUNTY_ID}-', ''), {year}, nullif(emv_land, 0), nullif(emv_bldg, 0), nullif(emv_total, 0), nullif(year_built, 0), sale_date, nullif(sale_value, 0), parcel_geom_id
+    select replace(pin, '{COUNTY_ID}-', ''), {year}, nullif(emv_land, 0), nullif(emv_bldg, 0), nullif(emv_total, 0), nullif(year_built, 0), sale_date, nullif(sale_value, 0), parcel_geom.id
     from parcel_raw_{year}, parcel_geom
-    where parcel_raw_{year}.geom = parcel_geom.parcel_geom_data
+    where parcel_raw_{year}.geom = parcel_geom.geom
       and upper({'city' if year < 2018 else 'ctu_name'}) = 'MINNEAPOLIS'
     """
     for year in range(2002, 2018)
 )
 
 parcel_load = f"""
-insert into parcel (parcel_id, parcel_year, parcel_emv_land, parcel_emv_building, parcel_emv_total, parcel_year_built, parcel_sale_date, parcel_sale_value, parcel_geom_id)
+insert into parcel (pid, year, emv_land, emv_building, emv_total, year_built, sale_date, sale_value, geom_id)
     {parcel_data}
     """
 print("Executing:", parcel_load)
--- etl/schema.sql
+++ etl/schema.sql
@@ -2,27 +2,28 @@ create extension if not exists postgis;
 
 drop table if exists parcel_geom cascade;
 create table parcel_geom (
-  parcel_geom_id serial primary key
-  , parcel_geom_data geometry(MultiPolygon, 26915) not null
+  id serial primary key
+  , geom geometry(MultiPolygon, 26915) not null
 );
-create index parcel_geom_data_idx on parcel_geom using gist(parcel_geom_data);
+create index parcel_geom_idx on parcel_geom using gist(geom);
 
 drop table if exists parcel;
 create table parcel (
-  parcel_pk serial primary key
-  , parcel_id text
-  , parcel_year int not null
+  id serial primary key
+  , pid text not null
+  , year int not null
 
-  , parcel_emv_land numeric
-  , parcel_emv_building numeric
-  , parcel_emv_total numeric
-  , parcel_year_built int
-  , parcel_sale_date date
-  , parcel_sale_value numeric
+  , emv_land numeric
+  , emv_building numeric
+  , emv_total numeric
+  , year_built int
+  , sale_date date
+  , sale_value numeric
 
-  , parcel_geom_id int references parcel_geom(parcel_geom_id)
+  , geom_id int references parcel_geom(id)
 );
 
-comment on column parcel.parcel_emv_land is 'Estimated Market Value, land';
-comment on column parcel.parcel_emv_building is 'Estimated Market Value, buildings';
-comment on column parcel.parcel_emv_total is 'Estimated Market Value, total (may be more than sum of land and building)';
+comment on column parcel.pid is 'Municipal parcel ID';
+comment on column parcel.emv_land is 'Estimated Market Value, land';
+comment on column parcel.emv_building is 'Estimated Market Value, buildings';
+comment on column parcel.emv_total is 'Estimated Market Value, total (may be more than sum of land and building)';
