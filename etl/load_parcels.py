d1cae373e23c7d09463c6a313d294286908be284
Jack Feser <jack.feser@gmail.com>
2024-08-05 11:00:24 -0400

    use correct srid when creating joined parcel table

--- etl/load_parcels.py
+++ etl/load_parcels.py
@@ -2,12 +2,13 @@
 
 import psycopg2
 
-PARCEL_YEARS = range(2002, 2018)
+from db import HOST, USER
+
+PARCEL_YEARS = range(2002, 2024)
 COUNTY_ID = "053"
 
-conn = psycopg2.connect(database="cities")
+conn = psycopg2.connect(host=HOST, user=USER, database="cities")
 cur = conn.cursor()
-
 
 with open("etl/schema.sql", "r") as f:
     cur.execute(f.read())
@@ -15,7 +16,7 @@ conn.commit()
 
 # select distinct geometry from all parcel tables
 distinct_geom = " union ".join(
-    f"select geom from parcel_raw_{year} where city = 'MINNEAPOLIS'"
+    f"select geom from parcel_raw_{year} where upper({'city' if year < 2018 else 'ctu_name'}) = 'MINNEAPOLIS'"
     for year in PARCEL_YEARS
 )
 parcel_geom_load = f"insert into parcel_geom (parcel_geom_data) {distinct_geom};"
@@ -26,13 +27,14 @@ conn.commit()
 # insert parcel data into parcel table
 parcel_data = " union all ".join(
     f"""
-    select replace(pin, '{COUNTY_ID}-', ''), {year}, emv_land, emv_bldg, emv_total, nullif(year_built, 0), sale_date, sale_value, parcel_geom_id
+    select replace(pin, '{COUNTY_ID}-', ''), {year}, nullif(emv_land, 0), nullif(emv_bldg, 0), nullif(emv_total, 0), nullif(year_built, 0), sale_date, nullif(sale_value, 0), parcel_geom_id
     from parcel_raw_{year}, parcel_geom
     where parcel_raw_{year}.geom = parcel_geom.parcel_geom_data
-      and city = 'MINNEAPOLIS'
+      and upper({'city' if year < 2018 else 'ctu_name'}) = 'MINNEAPOLIS'
     """
-    for year in PARCEL_YEARS
+    for year in range(2002, 2018)
 )
+
 parcel_load = f"""
 insert into parcel (parcel_id, parcel_year, parcel_emv_land, parcel_emv_building, parcel_emv_total, parcel_year_built, parcel_sale_date, parcel_sale_value, parcel_geom_id)
     {parcel_data}
