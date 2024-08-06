cf4b4a0bfb98f9fbd5f747198af1dc724e5edca3
Jack Feser <jack.feser@gmail.com>
2024-08-06 11:34:19 -0400

    reformat sql

--- etl/zip_schema.sql
+++ etl/zip_schema.sql
@@ -1,8 +1,11 @@
 drop table if exists zip_code;
+
 create table zip_code (
-  id serial primary key
-  , zip_code text not null
-  , valid daterange not null
-  , geom geometry(MultiPolygon, 4269) not null
+    id serial primary key
+    , zip_code text not null
+    , valid daterange not null
+    , geom geometry(MultiPolygon , 4269) not null
 );
-create index zip_code_geom_idx on zip_code using gist(geom);
+
+create index zip_code_geom_idx on zip_code using gist (geom);
+
--- scripts/clean.sh
+++ scripts/clean.sh
@@ -6,5 +6,7 @@ black cities/ tests/
 autoflake --remove-all-unused-imports --in-place --recursive ./cities ./tests
 
 nbqa black docs/guides/
-nbqa autoflake --remove-all-unused-imports --recursive --in-place docs/guides/ 
+nbqa autoflake --remove-all-unused-imports --recursive --in-place docs/guides/
 nbqa isort -in-place docs/guides/
+
+pg_format -c .pg_format -i etl/*.sql
