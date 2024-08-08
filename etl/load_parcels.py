d3fdee842d4cc52f0e95c4647be8e817ffd4fa55
Jack Feser <jack.feser@gmail.com>
2024-08-08 11:20:07 -0400

    clean up load_parcels.py

--- etl/load_parcels.py
+++ etl/load_parcels.py
@@ -1,44 +1,54 @@
 #!/usr/bin/env python
 
+import logging
 import psycopg2
 
 from db import HOST, USER
 
+log = logging.getLogger(__name__)
+
 PARCEL_YEARS = range(2002, 2024)
 COUNTY_ID = "053"
 
-conn = psycopg2.connect(host=HOST, user=USER, database="cities")
-cur = conn.cursor()
 
-with open("etl/parcel_schema.sql", "r") as f:
-    cur.execute(f.read())
-conn.commit()
+def main():
+    conn = psycopg2.connect(host=HOST, user=USER, database="cities")
+    cur = conn.cursor()
 
-# select distinct geometry from all parcel tables
-distinct_geom = " union ".join(
-    f"select geom from parcel_raw_{year} where upper({'city' if year < 2018 else 'ctu_name'}) = 'MINNEAPOLIS'"
-    for year in PARCEL_YEARS
-)
-parcel_geom_load = f"insert into parcel_geom (geom) {distinct_geom};"
-print("Executing:", parcel_geom_load)
-cur.execute(parcel_geom_load)
-conn.commit()
+    with open("etl/parcel_schema.sql", "r") as f:
+        cur.execute(f.read())
+    conn.commit()
 
-# insert parcel data into parcel table
-parcel_data = " union all ".join(
-    f"""
-    select replace(pin, '{COUNTY_ID}-', ''), '[{year-1}-01-01,{year}-01-01)'::daterange, nullif(emv_land, 0), nullif(emv_bldg, 0), nullif(emv_total, 0), nullif(year_built, 0), sale_date, nullif(sale_value, 0), parcel_geom.id
-    from parcel_raw_{year}, parcel_geom
-    where parcel_raw_{year}.geom = parcel_geom.geom
-      and upper({'city' if year < 2018 else 'ctu_name'}) = 'MINNEAPOLIS'
-    """
-    for year in PARCEL_YEARS
-)
+    # select distinct geometry from all parcel tables
+    distinct_geom = " union ".join(
+        f"select geom from parcel_raw_{year} where upper({'city' if year < 2018 else 'ctu_name'}) = 'MINNEAPOLIS'"
+        for year in PARCEL_YEARS
+    )
+    parcel_geom_load = f"insert into parcel_geom (geom) {distinct_geom};"
+    log.info("Executing: %s", parcel_geom_load)
+    cur.execute(parcel_geom_load)
+    conn.commit()
 
-parcel_load = f"""
-insert into parcel (pid, valid, emv_land, emv_building, emv_total, year_built, sale_date, sale_value, geom_id)
-    {parcel_data}
-    """
-print("Executing:", parcel_load)
-cur.execute(parcel_load)
-conn.commit()
+    # insert parcel data into parcel table
+    parcel_data = " union all ".join(
+        f"""
+        select replace(pin, '{COUNTY_ID}-', ''), '[{year-1}-01-01,{year}-01-01)'::daterange, nullif(emv_land, 0), nullif(emv_bldg, 0), nullif(emv_total, 0), nullif(year_built, 0), sale_date, nullif(sale_value, 0), parcel_geom.id
+        from parcel_raw_{year}, parcel_geom
+        where parcel_raw_{year}.geom = parcel_geom.geom
+          and upper({'city' if year < 2018 else 'ctu_name'}) = 'MINNEAPOLIS'
+        """
+        for year in PARCEL_YEARS
+    )
+
+    parcel_load = f"""
+    insert into parcel (pid, valid, emv_land, emv_building, emv_total, year_built, sale_date, sale_value, geom_id)
+        {parcel_data}
+        """
+    log.info("Executing: %s", parcel_load)
+    cur.execute(parcel_load)
+    conn.commit()
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    main()
