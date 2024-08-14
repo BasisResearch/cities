310a6da78ff4ce9ddad4dde4915bba7d995577a5
Jack Feser <jack.feser@gmail.com>
2024-08-14 17:12:40 -0400

    fix source

--- dbt/models/acs_block_group.sql
+++ dbt/models/acs_block_group.sql
@@ -23,12 +23,12 @@ census_block_groups as (
         {{ source('minneapolis_old', 'acs_bg_raw') }}
 )
 select
-    census_block_group_id
-    , year_
-    , name_
-    , value_
+    census_block_groups.census_block_group_id
+    , acs_bg.year_
+    , acs_bg.name_
+    , acs_bg.value_
 from
     acs_bg
     inner join census_block_groups using (statefp, countyfp, tractce, blkgrpce)
 where
-    to_date(acs_bg_raw.year_::text , 'YYYY') <@ census_block_groups.valid
+    to_date(acs_bg.year_::text , 'YYYY') <@ census_block_groups.valid
