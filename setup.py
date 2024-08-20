028368fb246fb2250072a086296f345ee26bb851
Jack Feser <jack.feser@gmail.com>
2024-08-20 13:14:58 -0400

    add dependencies to setup.py

--- setup.py
+++ setup.py
@@ -4,23 +4,27 @@ from setuptools import setup, find_packages
 VERSION = "0.1.0"
 
 TEST_REQUIRES = [
-            "pytest",
-            "pytest-cov",
-            "pytest-xdist",
-            "mypy",
-            "black",
-            "flake8",
-            "isort",
-            "nbval",
-            "nbqa",
-            "autoflake",
-        ]
+    "pytest",
+    "pytest-cov",
+    "pytest-xdist",
+    "mypy",
+    "black",
+    "flake8",
+    "isort",
+    "nbval",
+    "nbqa",
+    "autoflake",
+]
 
 DEV_REQUIRES = [
     "pyro-ppl>=1.8.5",
-    "torch",   "plotly.express", 
-    "scipy",  
-    "chirho", "graphviz",
+    "torch",
+    "plotly.express",
+    "scipy",
+    "chirho",
+    "graphviz",
+    "python-dotenv",
+    "google-cloud-storage",
 ]
 
 setup(
@@ -31,14 +35,20 @@ setup(
     author="Basis",
     url="https://www.basis.ai/",
     project_urls={
-    #     "Documentation": "",
+        #     "Documentation": "",
         "Source": "https://github.com/BasisResearch/cities",
     },
-    install_requires=["jupyter","pandas", "numpy", "scikit-learn","dill",  "plotly", "matplotlib>=3.8.2"],
-    extras_require={
-        "test": TEST_REQUIRES,
-        "dev": DEV_REQUIRES + TEST_REQUIRES 
-    },
+    install_requires=[
+        "jupyter",
+        "pandas",
+        "numpy",
+        "scikit-learn",
+        "dill",
+        "plotly",
+        "matplotlib>=3.8.2",
+    ],
+    extras_require={"test": TEST_REQUIRES, "dev": DEV_REQUIRES + TEST_REQUIRES},
     python_requires=">=3.10",
     keywords="similarity, causal inference, policymaking, chirho",
-    license="Apache 2.0",)
+    license="Apache 2.0",
+)
