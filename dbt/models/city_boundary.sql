a820807868005e02adf7a501c67fd7eb7a619058
Jack Feser <jack.feser@gmail.com>
2024-08-20 16:59:18 -0400

    fix misnamed field

--- dbt/models/city_boundary.sql
+++ dbt/models/city_boundary.sql
@@ -1,5 +1,5 @@
 select
-  ogc_id as city_boundary_id
+  ogc_fid as city_boundary_id
   , st_transform(geom, {{ var("srid") }}) as geom
 from
   {{ source('minneapolis', 'city_boundary_minneapolis') }}
