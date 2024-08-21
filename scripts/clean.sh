2526c3188369b4323df2167ace9148855b9953bb
Jack Feser <jack.feser@gmail.com>
2024-08-21 17:46:31 -0400

    remove unused line in clean.sh

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -8,5 +8,3 @@ autoflake --remove-all-unused-imports --in-place --recursive ./cities ./tests
 nbqa black docs/guides/
 nbqa autoflake --remove-all-unused-imports --recursive --in-place docs/guides/
 nbqa isort -in-place docs/guides/
-
-pg_format -c .pg_format -i etl/*.sql
