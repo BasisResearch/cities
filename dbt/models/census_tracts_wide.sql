f69a868bb110656894913d3b58a71e0ebe3d817a
Jack Feser <jack.feser@gmail.com>
2024-08-26 13:12:55 -0400

    unify approach to year replacement for demographic data

--- dbt/models/census_tracts_wide.sql
+++ dbt/models/census_tracts_wide.sql
@@ -11,7 +11,6 @@ in_city_boundary as (select * from {{ ref('census_tracts_in_city_boundary') }})
 , distance_to_transit as (select * from {{ ref('census_tracts_distance_to_transit') }})
 , parcel_area as (select * from {{ ref('census_tracts_parcel_area') }})
 , parking_limits as (select * from {{ ref('census_tracts_parking_limits') }})
-, acs_tract as (select * from {{ ref('acs_tract') }})
 , census_tracts as (
   select *
   from {{ ref('census_tracts') }}
@@ -20,28 +19,38 @@ in_city_boundary as (select * from {{ ref('census_tracts_in_city_boundary') }})
     and census_tract_id in (select census_tract_id from in_city_boundary)
 )
 
+-- Demographic data
+, acs_tract as (select * from {{ ref('acs_tract') }})
+, segregation_indexes as (
+  select census_tract, year_, 'segregation', segregation_index as value_
+  from {{ ref('segregation_indexes') }}
+  where distribution = 'annual_city'
+)
+, demographics as (
+  select * from acs_tract
+  union all
+  select * from segregation_indexes
+)
 -- Fill in data for 2011, 2012 using closest available year. Replace 2020 data
 -- with 2019 data to avoid pandemic effects.
-, acs_replace_years as (
-  select * from acs_tract where year_ != 2020
+, demographics_replace_years as (
+  select * from demographics where year_ != 2020
   union all
   select census_tract, 2020 as year_, name_, value_
-  from acs_tract where year_ = 2019
+  from demographics where year_ = 2019
   union all
-  -- select * from acs_tract
-  -- union all
   select census_tract, 2011 as year_, name_, value_
-  from acs_tract where year_ = 2013
+  from demographics where year_ = 2013
   union all
   select census_tract, 2012 as year_, name_, value_
-  from acs_tract where year_ = 2013
+  from demographics where year_ = 2013
 )
 , white as (
-  select * from acs_replace_years
+  select * from demographics_replace_years
   where name_ = 'B03002_003E' -- white non-hispanic population
 )
 , population as (
-  select * from acs_replace_years
+  select * from demographics_replace_years
   where name_ = 'B01003_001E' -- total population
 )
 , white_frac as (
@@ -49,9 +58,14 @@ in_city_boundary as (select * from {{ ref('census_tracts_in_city_boundary') }})
   from white inner join population using (census_tract, year_)
 )
 , income as (
-  select * from acs_replace_years
+  select * from demographics_replace_years
   where name_ = 'B19013_001E' -- median household income
 )
+, segregation as (
+  select * from demographics_replace_years
+  where name_ = 'segregation'
+)
+
 , raw_data as (
 select
   census_tracts.census_tract
@@ -65,6 +79,7 @@ select
   , parking_limits.mean_limit
   , white_frac.value_ as white
   , income.value_ as income
+  , segregation.value_ as segregation
 from
   census_tracts
   inner join housing_units using (census_tract_id)
@@ -72,6 +87,7 @@ from
   inner join distance_to_transit using (census_tract_id)
   inner join parcel_area using (census_tract_id)
   inner join parking_limits using (census_tract_id)
+  inner join segregation using (census_tract, year_)
   left join white_frac using (census_tract, year_)
   left join income using (census_tract, year_)
 )
@@ -88,9 +104,10 @@ select
   , white
   , income
   , mean_limit
+  , segregation
   , {{ standardize(['num_units', 'total_value', 'median_value',
                     'median_distance_to_transit', 'mean_distance_to_transit',
-                    'parcel_sqm', 'white', 'income', 'mean_limit' ]) }}
+                    'parcel_sqm', 'white', 'income', 'mean_limit', 'segregation' ]) }}
 from
   raw_data
 )
