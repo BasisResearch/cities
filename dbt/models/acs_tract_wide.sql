b77631093db8e47236c0a0cf67e61b906dc28f9d
Jack Feser <jack.feser@gmail.com>
2024-08-27 13:55:28 -0400

    fix up acs_tract_wide

--- dbt/models/acs_tract_wide.sql
+++ dbt/models/acs_tract_wide.sql
@@ -11,53 +11,35 @@
 
 with
 acs_tract as (select * from {{ ref('acs_tract') }})
-, acs_variables as (select * from {{ ref("acs_variables") }})
-, census_tracts_in_city_boundary as (
-  select * from {{ ref("census_tracts_in_city_boundary") }}
+, acs_variables as (select * from {{ ref('acs_variables') }})
+, census_tracts as (select * from {{ ref('census_tracts_in_city_boundary') }})
+, acs_tract_filtered as (
+  select acs_tract.*, description
+  from acs_tract
+  inner join census_tracts using (census_tract, year_)
+  inner join acs_variables on acs_tract.name_ = acs_variables.variable
 )
-, census_tracts as (
-  select
-    census_tract_id
-    , substring(geoidfq from 10) as geoidfq
-  from {{ ref("census_tracts") }}
-  where census_tract_id in (select census_tract_id from census_tracts_in_city_boundary)
-)
-, acs_tract_extended as (
-  select
-    acs_tract.census_tract_id
-    , census_tracts.geoidfq
-    , acs_tract.year_
-    , acs_tract.name_
-    , acs_tract.value_
-  from
-    acs_tract
-    inner join census_tracts using (census_tract_id)
-)
-
 , distinct_tracts_and_variables as (
   select distinct
-    geoidfq
+    census_tract
     , name_
-  from acs_tract_extended
+    , description
+  from acs_tract_filtered
 )
-
 select
-  acs_variables.description
-  , distinct_tracts_and_variables.geoidfq as tract_id
+  description
+  , census_tract as tract_id
 {% for year_ in years %}
   , "{{ year_ }}"
 {% endfor %}
-from
-distinct_tracts_and_variables
-inner join acs_variables
-      on distinct_tracts_and_variables.name_ = acs_variables.variable
+from distinct_tracts_and_variables
 {% for year_ in years %}
 left join
 (select
-  geoidfq
+  census_tract
   , name_
   , value_ as "{{ year_}}"
-from acs_tract_extended
+from acs_tract_filtered
 where year_ = {{ year_ }})
-using (geoidfq, name_)
+using (census_tract, name_)
 {% endfor %}
