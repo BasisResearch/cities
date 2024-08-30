9da68434ad17d6760baebd766182d6c4076be196
Jack Feser <jack.feser@gmail.com>
2024-08-30 15:18:45 -0400

    fix srid for api

--- dbt/models/api/api__high_frequency_transit_lines.sql
+++ dbt/models/api/api__high_frequency_transit_lines.sql
@@ -10,8 +10,8 @@
 select
   high_frequency_transit_line_id,
   valid,
-  geom,
-  blue_zone_geom,
-  yellow_zone_geom
+  st_transform(geom, 4269) as geom,
+  st_transform(blue_zone_geom, 4269) as blue_zone_geom,
+  st_transform(yellow_zone_geom, 4269) as yellow_zone_geom
 from
   {{ ref('high_frequency_transit_lines') }}
