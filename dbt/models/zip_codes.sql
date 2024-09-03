1b4878532ff7f6f0728a8430505f1399c11bbc4c
Jack Feser <jack.feser@gmail.com>
2024-09-03 16:40:45 -0400

    document and reorganize zip code models

--- dbt/models/zip_codes.sql
+++ dbt/models/zip_codes.sql
@@ -8,19 +8,26 @@
   )
 }}
 
+{% docs zip_codes %}
+
+Contains the geometry and metadata for all zip code tabulation areas (ZCTAs) in
+the United States.
+
+{% enddocs %}
+
 with
 zip_codes as (
 select
   zip_code,
   '[2020-01-01,)'::daterange as valid,
   geom
-from {{ ref('all_zip_codes_2020') }}
+from {{ ref('stg_zip_codes_2020') }}
 union all
 select
   zip_code,
   '[,2020-01-01)'::daterange as valid,
   geom
-from {{ ref('all_zip_codes_2010') }}
+from {{ ref('stg_zip_codes_2010') }}
 )
 select
   {{ dbt_utils.generate_surrogate_key(['zip_code', 'valid']) }} as zip_code_id,
