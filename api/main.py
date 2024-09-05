9c52b0b7b0d849e59c56e76595146d30017f741b
Jack Feser <jack.feser@gmail.com>
2024-09-05 14:28:23 -0400

    more api work

--- api/main.py
+++ api/main.py
@@ -1,7 +1,10 @@
 import os
 
 from fastapi import FastAPI, Depends
+from fastapi.middleware.cors import CORSMiddleware
+
 import psycopg2
+from typing import Dict
 
 from cities.deployment.tracts_minneapolis.predict import TractsModelPredictor
 
@@ -11,6 +14,19 @@ HOST = os.getenv("HOST")
 DATABASE = os.getenv("DATABASE")
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:5000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 def get_db() -> psycopg2.extensions.connection:
@@ -56,8 +72,17 @@ async def read_census_tracts(year: int, db=Depends(get_db)):
 @app.get("/predict")
 async def read_predict(
     samples=100,
-    intervention=None,
+    intervention: Dict[str, float] = None,
     predictor: TractsModelPredictor = Depends(get_predictor),
 ):
     result = predictor.predict(samples=samples, intervention=intervention)
-    return result.tolist()
+    result = [
+        {
+            "description": "predict",
+            "tract_id": str(result["census_tracts"][i].item()),
+            "2022": result["housing_units"][i].item(),
+        }
+        for i in range(len(result["census_tracts"]))
+    ]
+    print(result)
+    return result
