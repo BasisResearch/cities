591753b78b4403e929e2f0185770606df4aaf980
Jack Feser <jack.feser@gmail.com>
2024-09-09 11:25:25 -0400

    scripts and mechanisms

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 set -euxo pipefail
 
-isort --profile="black" cities/ tests/
+# isort suspended till the CI-vs-local issue is resolved
+# isort cities/ tests/
+
+isort --profile="black" ./cities ./tests
+black cities/ tests/
 autoflake --remove-all-unused-imports --in-place --recursive ./cities ./tests
-nbqa --nbqa-shell isort --profile="black" docs/guides/ 
-nbqa --nbqa-shell autoflake  --nbqa-shell --remove-all-unused-imports --recursive --in-place docs/guides/   
-black ./cities ./tests docs/guides/ 
+
+nbqa --nbqa-shell autoflake --remove-all-unused-imports --recursive --in-place docs/guides/
+nbqa --nbqa-shell isort --profile="black" docs/guides/
+black docs/guides/
