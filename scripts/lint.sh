19ed746eb7be3cdf25aa1b11951612c6e8bad810
rfl-urbaniak <rfl.urbaniak@gmail.com>
2024-09-11 10:23:57 -0400

    scripts, tests

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -2,11 +2,11 @@
 set -euxo pipefail
 
 mypy --ignore-missing-imports cities/
-#isort --check --diff cities/ tests/
+isort --profile="black" --check --diff cities/ tests/
 black --check cities/ tests/
 flake8 cities/ tests/ --ignore=E203,W503 --max-line-length=127
 
 
-nbqa autoflake -v --recursive --check docs/guides/
-#nbqa isort --check  docs/guides/
-nbqa black --check docs/guides/
+nbqa --nbqa-shell autoflake -v --recursive --check docs/guides/
+nbqa --nbqa-shell isort --profile="black" --check  docs/guides/
+black --check docs/guides/
