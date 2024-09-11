c0e6108b3e7ab2607388a0cd588b421f2b68b9be
Andy Zane <andy@basis.ai>
2024-09-11 16:24:05 -0400

    wip robust predict changes.

--- cities/deployment/tracts_minneapolis/robust_predict.py
+++ cities/deployment/tracts_minneapolis/robust_predict.py
@@ -13,7 +13,8 @@ from cities.modeling.zoning_models.zoning_tracts_model import TractsModel
 from cities.utils.data_grabber import find_repo_root
 from cities.utils.data_loader import select_from_data
 from copy import deepcopy
-from pyro.infer.autoguide import AutoDelta
+from pyro.infer.autoguide import AutoDiagonalNormal as AutoGuide
+# from pyro.infer.autoguide import AutoDelta as AutoGuide
 from chirho.observational.handlers import condition
 from utils import nonify_dict, map_subset_onto_obs
 from typing import Dict
@@ -42,13 +43,42 @@ from chirho.observational.handlers.predictive import PredictiveModel
 
 # can be disposed of once you access data in a different manner
 from cities.utils.data_grabber import find_repo_root
+from pyro.contrib.easyguide import easy_guide
 from cities.utils.data_loader import select_from_data
+import pyro.distributions as dist
 
 from chirho.indexed.ops import gather, IndexSet
 
+pyro.clear_param_store()
 pyro.settings.set(module_local_params=True)
 
 root = find_repo_root()
+
+
+# <Fake AutoDelta Workaround>
+def build_fake_delta(model, *args, **kwargs):
+
+    # TODO easier way to do this? Also 'data' is a plate...
+    with pyro.poutine.trace() as tr:
+        model()
+    latent_keys = [k for k, v in tr.get_trace().nodes.items() if not v['is_observed'] and k != 'data']
+
+    latent_dim = 0
+    for k in latent_keys:
+        latent_dim += tr.get_trace().nodes[k]['value'].numel()
+
+    params = torch.nn.Parameter(torch.zeros(latent_dim))
+
+    @easy_guide(model)
+    def fake_delta(self):
+        group = self.group(match=".*")
+        return group.sample("latents", dist.Normal(params, 1e-8).to_event(1))
+
+    fake_delta._parameters = {"latents_group": params}
+
+    return fake_delta
+
+# </Fake AutoDelta Workaround>
 
 # <Data Load and Prep>
 census_tracts_data_path = os.path.join(
@@ -80,19 +110,26 @@ kwargs = {
 }
 
 subset = select_from_data(data, kwargs)
+
+SUPER_CAT_LEVELS = ct_dataset_read.categorical_levels
 # </Data Load and Prep>
 
+
 # <Unconditioned Model>
-nonified_subset = nonify_dict(subset)
+class UnconditionedTractsModel(TractsModel):
+    def __init__(self, subset, n):
+        self.subset = subset
+        self.nonified_subset = nonify_dict(subset)
+        super().__init__(
+            **self.subset,
+            categorical_levels=SUPER_CAT_LEVELS
+        )
 
-# Initialize the tracts model with the full dataset. This establishes the full categorical leveling.
-_og_model = TractsModel(
-    **subset,
-    categorical_levels=ct_dataset_read.categorical_levels
-)
+        self.n = n
 
-# Passing the nonified subset to the forward ensures that all obs= get set to None.
-unconditioned_model = partial(_og_model, **nonified_subset)
+    # noinspection PyMethodOverriding
+    def forward(self):
+        return super().forward(n=self.n, **self.nonified_subset)
 # </Unconditioned Model>
 
 # <Conditioned Model>
@@ -112,35 +149,59 @@ subset_as_obs = map_subset_onto_obs(
 )
 full_n = next(iter(subset_as_obs.values())).shape[0]
 
+# Passing the nonified subset to the forward ensures that all obs= get set to None.
+full_unconditioned_model = UnconditionedTractsModel(subset, n=full_n)
+
+# DEBUG
+# full_unconditioned_model()
+
+with pyro.plate("outer", size=13, dim=-2):
+    result = full_unconditioned_model()
+# Sanity check.
+assert result.shape == (13, 816)
+
+with pyro.plate("outer", size=14, dim=-3):
+    result = full_unconditioned_model()
+# Sanity check.
+assert result.shape == (14, 1, 816)
+# /DEBUG
+
 full_conditioned_model = condition(
-    partial(unconditioned_model, n=full_n),
+    full_unconditioned_model,
     data=subset_as_obs
 )
 # </Conditioned Model>
 
-# <Training>
+# <DEBUG check fake delta>
+build_fake_delta(full_conditioned_model)
+# </DEBUG check fake delta>
 
+# <Training>
+LR = 0.005
+NUM_STEPS = 1000
 guide = run_svi_inference(
     full_conditioned_model,
-    n_steps=1000,
-    lr=0.1,
-    vi_family=AutoDelta,
-    plot=False
+    n_steps=NUM_STEPS,
+    lr=LR,
+    vi_family=AutoGuide,
+    # guide=build_fake_delta(full_conditioned_model),
+    plot=True
 )
 
 # </Training>
 
 # <Generate Data>
 # Construct the dataset sizes.
-sizes = [100, 300, 900]  # , 2700, 8100, 24300]
+sizes = [100, 200, 300, 500, 900, 1200, 1900, 2700, 5000, 7500, 10000]  # , 2700, 8100, 24300]
 
 # Trace the latents for replay (we're using a delta, so only one is needed).
 map_params = guide()
 
 datasets = []
 for size in sizes:
+    unconditioned_model_of_size = UnconditionedTractsModel(subset, n=size)
     predictive = Predictive(
-        model=partial(unconditioned_model, n=size),
+        model=unconditioned_model_of_size,
         guide=guide,
         num_samples=1,
     )
@@ -166,13 +227,9 @@ for size in sizes:
 
 # <Functional>
 class TargetFunctional(torch.nn.Module):
-    def __init__(self, model, guide, num_tracts, num_mc=100):
+    def __init__(self, model, num_tracts, num_mc=100):
         super().__init__()
-        self.predictive_model = Predictive(
-            model=model,
-            guide=guide,
-            num_samples=num_mc
-        )
+        self.model = model
         # Keeping it simple for validation purposes. Removing limits entirely vs. requiring limits.
         self.no_limit = torch.zeros(num_tracts)
         self.full_limit = torch.ones(num_tracts)
@@ -180,33 +237,29 @@ class TargetFunctional(torch.nn.Module):
     def forward(self):
         with MultiWorldCounterfactual() as mwc:
             with do(actions={"limit": (self.no_limit, self.full_limit)}):
-                hu = self.predictive_model()["housing_units"]
+                hu = self.model()
 
             hu0 = gather(hu, IndexSet(limit={1}, event_dim=0))
             hu1 = gather(hu, IndexSet(limit={2}, event_dim=0))
 
         # We want hu0 first, as that's the one we hypothesize will induce more units. This is the effect of
-        #  removing the the limit.
+        #  removing the limit.
         return (hu0 - hu1).mean(dim=0, keepdim=True).mean(dim=-1, keepdim=True).squeeze()
-
 # </Functional>
 
 
 # <Ground Truth>
-
-ground_truth_functional = partial(
-    TargetFunctional,
-    guide=guide,
+ground_truth_estimator = TargetFunctional(
+    PredictiveModel(full_unconditioned_model, guide),
     num_tracts=full_n,
     num_mc=1000
 )
-ground_truth_estimator = ground_truth_functional(partial(unconditioned_model, n=full_n))
 ground_truth = ground_truth_estimator()
 print("GT Housing Unit Delta Between Full and No Limits: ", ground_truth.item())
 # </Ground Truth>
 
 
-def get_correction(unconditioned_model, data: Dict[str, torch.Tensor]):
+def get_correction(data: Dict[str, torch.Tensor]):
 
     # Split the data into train and correction.
     n = next(iter(data.values())).shape[0]
@@ -216,32 +269,36 @@ def get_correction(unconditioned_model, data: Dict[str, torch.Tensor]):
     train_data = {k: v[:train_n] for k, v in data.items()}
     correction_data = {k: v[train_n:] for k, v in data.items()}
 
+    unconditioned_train_model = UnconditionedTractsModel(subset, n=train_n)
+    unconditioned_correction_model = UnconditionedTractsModel(subset, n=correction_n)
+
     # Find the MAP.
     conditioned_model = condition(
-        partial(unconditioned_model, n=train_n),
+        unconditioned_train_model,
         data=train_data
     )
 
     guide = run_svi_inference(
         conditioned_model,
-        n_steps=1000,
-        lr=0.1,
-        vi_family=AutoDelta,
+        n_steps=NUM_STEPS,
+        lr=LR,
+        vi_family=AutoGuide,
+        # guide=build_fake_delta(conditioned_model),
         plot=False
     )
 
-    model_for_plugin = partial(unconditioned_model, n=train_n)
-    model_for_correction = partial(unconditioned_model, n=correction_n)
+    model_for_plugin = PredictiveModel(unconditioned_train_model, guide)
+    model_for_correction = PredictiveModel(unconditioned_correction_model, guide)
 
     functional_for_plugin = partial(
         TargetFunctional,
-        guide=guide,
-        num_tracts=train_n
+        num_tracts=train_n,
+        num_mc=100
     )
     functional_for_correction = partial(
         TargetFunctional,
-        guide=guide,
-        num_tracts=correction_n
+        num_tracts=correction_n,
+        num_mc=100
     )
 
     estimator_for_plugin = functional_for_plugin(model_for_plugin)
@@ -253,33 +310,60 @@ def get_correction(unconditioned_model, data: Dict[str, torch.Tensor]):
         pointwise_influence=False
     )
 
-    raise NotImplementedError(
-        "model_for_correction isn't a torch module..."
-    )
     correction_estimator = influence_functional(model_for_correction)
-    with MonteCarloInfluenceEstimator(
-        num_samples_outer=1000,
-        num_samples_inner=1,
-    ):
-        correction = correction_estimator()
+    with torch.no_grad():
+        with MonteCarloInfluenceEstimator(
+            num_samples_outer=1000,
+            num_samples_inner=33,
+        ):
+            correction = correction_estimator()
 
-    return plugin, plugin + correction
+    print(f"Plugin Estimate: {plugin_estimate.item()}")
+    print(f"Correction: {correction.item()}")
+    print()
+
+    return plugin_estimate, plugin_estimate + correction
 
 
 # <Correction>
 
-plugins_and_corrections = []
+plugins_and_corrections = [get_correction(dataset) for dataset in datasets]
 
-for dataset in datasets:
-    plugin, corrected = get_correction(unconditioned_model, dataset)
-    plugins_and_corrections.append((plugin, corrected))
+print(plugins_and_corrections)
 
 # </Correction>
 
 # <Plot>
 
 # Plot the plugin and correction agains the ground truth for each dataset size.
-raise NotImplementedError()
+# We want a few plots.
+# 1. A plot with the groundtruth vline, plugin, and corrected estimate.
+plt.figure()
+plt.axhline(ground_truth.item(), color='red', label="Ground Truth")
+plt.plot(sizes, [x[0].item() for x in plugins_and_corrections], label="Plugin")
+plt.plot(sizes, [x[1].item() for x in plugins_and_corrections], label="Corrected", linestyle='--')
+plt.xlabel("Dataset Size")
+plt.ylabel("Housing Units Delta")
+plt.legend()
+
+# 2. A plot of the absolute bias of the plugin and the corrected estimate.
+plt.figure()
+plt.plot(sizes, [abs(x[0].item() - ground_truth.item()) for x in plugins_and_corrections], label="Plugin")
+plt.plot(sizes, [abs(x[1].item() - ground_truth.item()) for x in plugins_and_corrections], label="Corrected", linestyle='--')
+plt.xlabel("Dataset Size")
+plt.ylabel("Absolute Bias")
+plt.legend()
+
+# 3. A plot of the correction term and what the correction shouuld be (diff between plugin and ground truth).
+plt.figure()
+plt.plot(sizes, [x[1].item() - x[0].item() for x in plugins_and_corrections], label="Correction")
+plt.plot(sizes, [ground_truth.item() - x[0].item() for x in plugins_and_corrections], label="Perfect Correction", linestyle='--')
+plt.xlabel("Dataset Size")
+plt.ylabel("Correction")
+plt.legend()
+
+plt.show()
+
 
 # </Plot>
 
