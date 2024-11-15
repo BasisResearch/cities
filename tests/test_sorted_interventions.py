1dd1145d86c111f5bf9ac3d946b7a3c6b4ed05b9
rfl-urbaniak <rfl.urbaniak@gmail.com>
2024-11-15 11:46:31 -0500

    format, lint

--- tests/test_sorted_interventions.py
+++ tests/test_sorted_interventions.py
@@ -2,10 +2,10 @@ import os
 
 import dill
 import numpy as np
+import pytest
 
 from cities.utils.data_grabber import DataGrabber, find_repo_root, list_interventions
 from cities.utils.percentiles import transformed_intervention_from_percentile
-import pytest
 
 smoke_test = "CI" in os.environ
 
@@ -13,6 +13,7 @@ if smoke_test:
     pytest.skip(
         "Skipping all tests in this file during smoke tests", allow_module_level=True
     )
+
 
 def test_sorted_interventions_present():
     root = find_repo_root()
